3b960c0e5ccf041b0fb824fd967a6d30eabc2e0e
Telma gama <telmagamasilva@gmail.com>
2018-06-03 00:16:11 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+
+            contaDaGabriela.titular = "Gabriela";
+            contaDaGabriela.agencia = 862;
+            contaDaGabriela.numero = 86345;
+            contaDaGabriela.saldo = 100;
+
+
+            Console.WriteLine(contaDaGabriela.titular);
+            Console.WriteLine("Agencia: " + contaDaGabriela.agencia);
+            Console.WriteLine("Numero: " + contaDaGabriela.numero);
+            Console.WriteLine("Saldo: " + contaDaGabriela.saldo);
+
+            contaDaGabriela.saldo += 200;
+            Console.WriteLine("Saldo: " + contaDaGabriela.saldo);
+
+            Console.ReadLine();
+        }
+    }
+}
