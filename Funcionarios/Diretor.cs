de62b5ce4d5f449125a657b22bbb82c8fe9c21e5
Telma gama <telmagamasilva@gmail.com>
2018-06-14 10:30:10 -0300

    ByteBank

---
+++ Funcionarios/Diretor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Funcionarios
+{
+    public class Diretor : Funcionario  // herança , O diretor está herdando os atribudos do Funcionario
+    {
+       
+        public override double GetBonificacao()
+        {
+           
+            return Salario + base.GetBonificacao();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿
+using ByteBank.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
+
+
+            Funcionario carlos = new Funcionario();
+
+            carlos.Nome = "Carlos";
+            carlos.CPF = "345.554.3332-44";
+            carlos.Salario = 2000;
+
+            gerenciador.Registrar(carlos);
+
+            Diretor roberta = new Diretor(); // instancia do objeto
+
+            roberta.Nome = "Roberta";
+            roberta.CPF = "345.678.943-32";
+            roberta.Salario = 5000;
+
+            Funcionario robertaTeste = roberta;
+
+            Console.WriteLine("Bonficacao de uma referencia de Diretor: " + roberta.GetBonificacao());
+            Console.WriteLine("Bonificacao de uma referencia de Funcionario: " + robertaTeste.GetBonificacao());
+
+            gerenciador.Registrar(roberta);
+            
+
+            Console.WriteLine(carlos.Nome);
+            Console.WriteLine(carlos.GetBonificacao());
+
+            Console.WriteLine(roberta.Nome);
+            Console.WriteLine(roberta.GetBonificacao());
+
+            Console.WriteLine("Total de bonificações: " + gerenciador.GetTotalBonificacao());
+
+            Console.ReadLine();
+        }
+    }
+}
